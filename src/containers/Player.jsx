c6578b3db7634764b1ff7ddd051923cca8c192ca
ivanyovera <ivanyoveraa@gmail.com>
2021-03-20 18:00:36 -0300

    uso de useLayoutEffect

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getVideoSource } from "../actions";
